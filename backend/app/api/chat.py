944f575bc5b8009f692a9381b3a2aec70cf33b2e
gaykun1 <139235847+gaykun1@users.noreply.github.com>
2026-01-04 21:02:22 +0200

    (feat): pinecone implementation + manual chain creating

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -1,11 +1,11 @@
 from ..core.llm import llm
 from fastapi import APIRouter
 from .schemas.chat import ChatRequest
-from langchain_chroma import Chroma
-from langchain_community.chains import PebbloRetrievalQA
+from langchain_pinecone import PineconeVectorStore
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv
-from os import getenv
 
 load_dotenv()
 
@@ -14,19 +14,32 @@ router = APIRouter(prefix="/chat", tags=["chat"])
 
 @router.post("/")
 async def chat_with_resume(req: ChatRequest):
-    vectordb = Chroma(
-        embedding_function=OpenAIEmbeddings(),
-        persist_directory=f"./chroma/{req.user_id}",
+    vectordb = PineconeVectorStore.from_existing_index(
+        index_name="resume-index",
+        embedding=OpenAIEmbeddings(),
+        namespace=req.user_id,
     )
     retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-    qa_chain = PebbloRetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=retriever,
-        chain_type="stuff",
-        app_name="AI Recruiter",
-        description="RAG QA chain for candidate resumes",
-        owner=getenv("OWNER_NAME"),
-    )
-    result = qa_chain.run(req.question)
+    prompt = ChatPromptTemplate.from_template("""
+    You are an AI recruiter.
+    Answer the question using ONLY the context below.
 
-    return {"answer": result}
+    Context:
+    {context}
+
+    Question:
+    {question}
+    """)
+
+    chain = (
+        {
+            "context": retriever,
+            "question": RunnablePassthrough()
+        }
+        | prompt
+        | llm
+    )
+
+    result = chain.invoke(req.question)   
+
+    return {"answer": result.content}
--- backend/app/rag/vector_store.py
+++ backend/app/rag/vector_store.py
@@ -1,19 +1,36 @@
-from langchain_chroma import Chroma
+from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import pinecone
+from pinecone import ServerlessSpec
+from os import getenv
 
-def create_vector_store(docs,user_id:str):
+index_name = getenv("INDEX_NAME")
+
+pc = pinecone.Pinecone(api_key=getenv("PINECONE_API_KEY"))
+
+#   if index hasn`t been created yet
+
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=1536,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+
+def create_vector_store(docs, user_id: str):
+
     embeddings = OpenAIEmbeddings()
     # splitter config
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=150
-    )
-    
+    splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=150)
+
     chunks = splitter.split_documents(docs)
-    vectordb = Chroma.from_documents(
+    vectordb = PineconeVectorStore.from_documents(
         embedding=embeddings,
-        persist_directory=f"./chroma/{user_id}",
-        documents = chunks
+        index_name=index_name,
+        documents=chunks,
+        namespace=user_id,
     )
     return vectordb
